848b2cfc20c73066b95c2e68524562ad8c5fd333
Pramod CN <pramod.n@abylle.com>
2023-08-23 13:14:49 +0530

    S

---
+++ SdbProject/SdbProject/dbo/Tables/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId]   INT  NOT NULL,
+    [ProductName] TEXT NULL,
+    [Price] MONEY NULL, 
+    [Description] TEXT NULL, 
+    PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
